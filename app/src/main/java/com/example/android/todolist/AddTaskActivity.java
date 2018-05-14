3b948b9bb406aeefdd03de92c076112c096dd3d3
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2018-05-14 21:07:52 +0300

    Save Task

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -24,7 +24,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
+import com.example.android.todolist.database.AppDatabase;
 import com.example.android.todolist.database.TaskEntry;
+
+import java.util.Date;
 
 
 public class AddTaskActivity extends AppCompatActivity {
@@ -48,11 +51,15 @@ public class AddTaskActivity extends AppCompatActivity {
 
     private int mTaskId = DEFAULT_TASK_ID;
 
+    private AppDatabase mDb;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
         initViews();
+
+        mDb = AppDatabase.getsInstance(getApplicationContext());
 
         if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
             mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
@@ -103,8 +110,15 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onSaveButtonClicked() {
-        // Not yet implemented
+        String description = mEditText.getText().toString();
+        int priority = getPriorityFromViews();
+        Date date = new Date();
+
+        TaskEntry taskEntry = new TaskEntry(description, priority, date);
+        mDb.taskDao().insertTask(taskEntry);
+        finish();
     }
+
 
     /**
      * getPriority is called whenever the selected priority needs to be retrieved
